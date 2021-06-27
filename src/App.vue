da6659ce13e7c4ebc4b5c89a1e427ccf0bc0c878
chaoting <02115214@gm.scu.edu.tw>
2021-06-28 03:22:54 +0800

    use vuex

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="nav">
+    {{ greeting }}<br />
+    {{ price }}<br />
+    {{ product }}<br />
+    {{ quantity }}<br />
+    <input v-model="qty" />
+    <button @click="setQuantity(qty)">mutations</button><br />
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
@@ -28,3 +34,28 @@
   }
 }
 </style>
+<script>
+// 匯入 vuex 提供的工具，回傳物件，內部放function
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
+export default {
+  name: "App",
+  data() {
+    return {
+      qty: 0,
+    };
+  },
+  created() {
+    this.fetchProductData({ id: "008" });
+  },
+  computed: {
+    // 透過 mapState 取得 store 內的 state，透過展開物件內容，避免搶佔computed
+    ...mapState(["greeting", "price", "product", "quantity"]),
+    // 透過 mapGetters 取得 store 內的 getters，透過展開物件內容，避免搶佔computed
+    ...mapGetters(["handleSellingPrice"]),
+  },
+  methods: {
+    ...mapMutations(["setQuantity"]),
+    ...mapActions(["fetchProductData"]),
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,42 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    greeting: "Hello Word!",
+    price: 333,
+    product: {},
+    quantity: 100,
+  },
+  getters: {
+    discount(state) {
+      // 當庫存超過50打八折，否則打九折
+      return state.quantity > 50 ? 0.8 : 0.9;
+    },
+    // getters裡，可以取得state,getters
+    handleSellingPrice(state, getters) {
+      //售價打八折
+      return state.price * getters.discount;
+    },
+  },
+  // 修改 state 的唯一方式是透過 commit 某 mutations
+  mutations: {
+    // CB，在子元件觸發，將子元件傳來的值，更新到對應的狀態中
+    setQuantity(state, payload) {
+      state.quantity = payload;
+    },
+    setProduct(state, payload) {
+      state.product = payload;
+    },
+  },
+  actions: {
+    // eslint-disable-next-line no-unused-vars
+    handleFetchProductData(context, payload) {
+      //可以進行異步執行
+      fetch("...")
+        .then((res) => res.json())
+        //將結果提交給mutations已更改狀態
+        .then((data) => context.commit("setProduct", data));
+    },
+  },
   modules: {},
 });
